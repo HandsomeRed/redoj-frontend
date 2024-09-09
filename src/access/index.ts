381121ab020ad0060952b6a5f1d9b0ccc0f7f848
TinyRed <978964494@qq.com>
2024-09-09 22:52:04 +0800

    Initial commit

---
+++ src/access/index.ts
@@ -0,0 +1,26 @@
+import router from "@/router";
+import store from "@/store";
+import ACCESS_ENUM from "@/access/accessEnum";
+import checkAccess from "@/access/checkAccess";
+
+router.beforeEach(async (to, from, next) => {
+  const loginUser = store.state.user.loginUser;
+
+  if (!loginUser || !loginUser.userRole) {
+    // await 是为了等用户登录成功以后，再执行后续的代码
+    await store.dispatch("user/getLoginUser");
+  }
+  const needAccess = (to.meta?.access as string) ?? ACCESS_ENUM.NOT_LOGIN;
+  // 要跳转的页面需要登录
+  if (needAccess !== ACCESS_ENUM.NOT_LOGIN) {
+    if (!loginUser) {
+      next(`/user/login?redirect=${to.fullPath}`);
+      return;
+    }
+    if (!checkAccess(loginUser, needAccess)) {
+      next("/noAuth");
+      return;
+    }
+  }
+  next();
+});
